37b5a016b93e51916eb76eaea27534bf33423e87
Mingli Yuan <mingli.yuan@gmail.com>
2013-05-02 23:39:30 +0800

    change sphere api

--- src/main/java/org/wahlque/sphere/Sphere.java
+++ src/main/java/org/wahlque/sphere/Sphere.java
@@ -2,54 +2,37 @@ package org.wahlque.sphere;
 
 public interface Sphere {
 
+    // type
     public static final String SCALAR = "scalar";
     public static final String VECTOR = "vector";
+    // kind
     public static final String SURFICIAL = "surficial";
     public static final String SPATIAL = "spatial";
+    // variability
     public static final String CONSTANT = "constant";
     public static final String TEMPORAL = "temporal";
 
-    public String create(String target, String type, String[] kinds, int scale,
-            int zscale, double[] zaxis, String expression);
+    public String mkspace(int scale, int zscale, double[] zaxis);
 
-    public String create(String target, String expression);
+    public String mkpartition(int number);
+
+    public String mksubspace(int index);
+
+    public String mkfield(String target, String type, String kind,
+            String variability, String expression);
 
     public String type(String target);
 
-    public String[] kinds(String target);
+    public String kind(String target);
+
+    public String variability(String target);
 
     public double[] get(String target, double[] points);
 
-    public double[] query(String target, String condition);
-
     public int put(String target, double[] points, double[] values);
 
-    public int add(String target, String source1, String source2);
+    public String eval(String target, String expression);
 
-    public int minus(String target, String source1, String source2);
-
-    public int expand(String target, String source, double scale);
-
-    public int product(String target, String source1, String source2);
-
-    public int inner(String target, String source1, String source2);
-
-    public int cross(String target, String source1, String source2);
-
-    public int dot(String target, String source);
-
-    public int ddot(String target, String source);
-
-    public int grad(String target, String source);
-
-    public int div(String target, String source);
-
-    public int curl(String target, String source);
-
-    public int laplacian(String target, String source);
-
-    public double max(String target);
-
-    public double sum(String target);
+    public double[] query(String target, String condition);
 
 }
