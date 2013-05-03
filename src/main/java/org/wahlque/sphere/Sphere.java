af9b5378ea3381b68ad82018ddf48b02afaf7e37
Mingli Yuan <mingli.yuan@gmail.com>
2013-05-03 08:34:22 +0800

    new api

--- src/main/java/org/wahlque/sphere/Sphere.java
+++ src/main/java/org/wahlque/sphere/Sphere.java
@@ -5,18 +5,16 @@ public interface Sphere {
     // type
     public static final String SCALAR = "scalar";
     public static final String VECTOR = "vector";
+
     // kind
     public static final String SURFICIAL = "surficial";
     public static final String SPATIAL = "spatial";
+
     // variability
     public static final String CONSTANT = "constant";
     public static final String TEMPORAL = "temporal";
 
     public String mkspace(int scale, int zscale, double[] zaxis);
-
-    public String mkpartition(int number);
-
-    public String mksubspace(int index);
 
     public String mkfield(String target, String type, String kind,
             String variability, String expression);
@@ -29,10 +27,18 @@ public interface Sphere {
 
     public double[] get(String target, double[] points);
 
+    public double[] query(String target, String condition);
+
     public int put(String target, double[] points, double[] values);
 
-    public String eval(String target, String expression);
+    public int eval(String target, String expression);
 
-    public double[] query(String target, String condition);
+    public int[] mkpart(int number);
+
+    public int mksubspace(int parind);
+
+    public int backup(String target, int parind, double[] data);
+
+    public double[] exchange(String target, int parind);
 
 }
