8d7d9fd92476c908fcd1e7048fb921c0a41b1226
Mingli Yuan <mingli.yuan@gmail.com>
2013-05-05 14:52:47 +0800

    add scala version of vector

---
+++ src/main/scala/org/wahlque/math/Basic.scala
@@ -0,0 +1,17 @@
+package org.wahlque.math
+
+object Basic {
+
+  def id(x: Double): Double = x
+
+  def zero(x: Double): Double = 0
+
+  def one(x: Double): Double = 1
+
+  def sqr(x: Double): Double = x * x
+
+  def add(x: Double, y: Double): Double = x + y
+
+  def product(x: Double, y: Double): Double = x * y
+
+}
---
+++ src/main/scala/org/wahlque/math/geometry/Vector3.scala
@@ -0,0 +1,41 @@
+package org.wahlque.math.geometry
+
+import scala.math.{ sqrt, sin, cos, random => rnd, Pi }
+
+class Vector3(a: Double, b: Double, c: Double) {
+
+  val x: Double = a
+  val y: Double = b
+  val z: Double = c
+
+  def norm: Double = sqrt(x * x + y * y + z * z)
+
+  def +(that: Vector3): Vector3 = new Vector3(x + that.x, y + that.y, z + that.z)
+
+  def -(that: Vector3): Vector3 = new Vector3(x - that.x, y - that.y, z - that.z)
+
+  def *(factor: Double): Vector3 = new Vector3(x * factor, y * factor, z * factor)
+
+  def ^(that: Vector3): Vector3 = new Vector3(y * that.z - z * that.y, z * that.x - z * that.z, x * that.y - y * that.x)
+
+  def %(): Vector3 = if (norm == 0) Vector3.zero else new Vector3(x / norm, y / norm, z / norm)
+
+  def $(that: Vector3): Double = x * that.x + y * that.y + z * that.z
+
+}
+
+object Vector3 {
+
+  val zero = new Vector3(0, 0, 0)
+
+  def -(vec: Vector3): Vector3 = new Vector3(-vec.x, -vec.y, -vec.z)
+
+  def product(a: Vector3, b: Vector3, c: Vector3): Double = b ^ c $ a
+
+  def random(r: Double = 1.0): Vector3 = {
+    val theta = (rnd - 0.5) * Pi
+    val phi = rnd * Pi * 2
+    new Vector3(r * cos(phi) * cos(theta), r * cos(phi) * sin(theta), r * sin(phi))
+  }
+
+}
